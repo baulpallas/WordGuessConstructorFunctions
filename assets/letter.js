142d9f0cec1ae09a288440e7ebf22b57da335ce9
Paul Ballas <paballas51@gmail.com>
2020-01-24 00:03:16 -0500

    first commit

---
+++ assets/letter.js
@@ -0,0 +1,11 @@
+var inquirer = require("inquirer");
+let Letter = (guess, rightGuess, wrongGuess) => {
+  this.guess = guess;
+  this.rightGuess = rightGuess;
+  this.wrongGuess = wrongGuess;
+
+  this.guessRight = () => {
+    if (guess === rightGuess) {
+    }
+  };
+};
